03a21d1cc71476b7fe965623f96510f06cf71040
alexrebula <alex@alexrebula.com>
2021-10-23 00:06:05 +1100

    ADD: Icon component with Font Awesome

--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/atoms/icon/icon.component.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/atoms/icon/icon.component.ts
@@ -1,12 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+    faCoffee,
+    faCog,
+    faDog,
+    faBrain,
+    faCross,
+    faLevelDownAlt,
+    faUserFriends,
+    faRunning,
+    faChartLine,
+    faDna,
+    faSuitcase,
+    faSwatchbook,
+    faChalkboardTeacher,
+    faIndustry,
+    faBandAid,
+    faSpa,
+    faStar,
+    faBus,
+    faThumbsUp,
+    faAngleUp,
+    faAngleDown,
+    faAngleLeft,
+    faAngleRight,
+    faCheck,
+    faCircleNotch,
+} from '@fortawesome/free-solid-svg-icons';
+import { IICon } from './_interfaces/icon.interfaces';
+import { IButtonIconSizeEnum } from '../button/_interfaces/button.enums';
 @Component({
     selector: 'gee-icon',
     templateUrl: './icon.component.html',
     styleUrls: ['./icon.component.scss'],
 })
 export class IconComponent implements OnInit {
-    constructor() {}
+    @Input()
+    icon: IICon | undefined;
 
-    ngOnInit(): void {}
+    iconName: any;
+    iconSize: any;
+
+    constructor(private library: FaIconLibrary) {}
+
+    ngOnInit(): void {
+        if (this.icon && this.icon.name) {
+            this.iconName = this.icon.name;
+        }
+
+        if (this.icon && this.icon.size) {
+            this.iconSize = this.icon.size;
+        }
+        this.library.addIcons(
+            faCoffee,
+            faCog,
+            faDog,
+            faBrain,
+            faCross,
+            faLevelDownAlt,
+            faUserFriends,
+            faRunning,
+            faChartLine,
+            faDna,
+            faSuitcase,
+            faSwatchbook,
+            faChalkboardTeacher,
+            faIndustry,
+            faBandAid,
+            faSpa,
+            faStar,
+            faBus,
+            faThumbsUp,
+            faAngleUp,
+            faAngleDown,
+            faAngleLeft,
+            faAngleRight,
+            faCheck
+        );
+
+        if (this.icon && !this.icon.size) {
+            this.icon.size = IButtonIconSizeEnum.fa2x;
+        }
+    }
 }
--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/atoms/icon/icon.module.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/atoms/icon/icon.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from './icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     declarations: [IconComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FontAwesomeModule],
     exports: [IconComponent],
 })
 export class IconModule {}
