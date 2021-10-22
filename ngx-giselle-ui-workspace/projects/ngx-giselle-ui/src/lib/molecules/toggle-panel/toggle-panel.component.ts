2f8ea392b72fd8fda7d6b6bc1c4ddec577e15a62
alexrebula <alex@alexrebula.com>
2021-10-23 00:19:05 +1100

    ADD: Toggle panel component

--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/molecules/toggle-panel/toggle-panel.component.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/molecules/toggle-panel/toggle-panel.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+    IButtonColorEnum,
+    IButtonIconSizeEnum,
+    IFontAwesomeIconEnum,
+} from '../../atoms/button/_interfaces/button.enums';
+import { IICon } from '../../atoms/icon/_interfaces/icon.interfaces';
 
 @Component({
     selector: 'gee-toggle-panel',
@@ -6,7 +12,40 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./toggle-panel.component.scss'],
 })
 export class TogglePanelComponent implements OnInit {
+    @Input()
+    title: string | undefined;
+
+    @Input()
+    alwaysExpanded: boolean | undefined = false;
+
+    iconSize = IButtonIconSizeEnum.fa2x;
+
+    carouselControlButtonColor: IButtonColorEnum | undefined =
+        IButtonColorEnum.primary;
+
+    icon: IICon | undefined = {
+        name: IFontAwesomeIconEnum.faAngleDown,
+        size: this.iconSize,
+    };
+
+    isExpanded = true;
+
+    toggleButtonColor = IButtonColorEnum.white;
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    onTogglePanelButtonClick() {
+        this.isExpanded = !this.isExpanded;
+        this.icon = this.isExpanded
+            ? {
+                  name: IFontAwesomeIconEnum.faAngleDown,
+                  size: this.iconSize,
+              }
+            : {
+                  name: IFontAwesomeIconEnum.faAngleRight,
+                  size: this.iconSize,
+              };
+    }
 }
--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/molecules/toggle-panel/toggle-panel.module.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/molecules/toggle-panel/toggle-panel.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TogglePanelComponent } from './toggle-panel.component';
+import { IconModule } from '../../atoms/icon/icon.module';
+import { ButtonModule } from '../../atoms/button/button.module';
 
 @NgModule({
     declarations: [TogglePanelComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, ButtonModule, IconModule],
     exports: [TogglePanelComponent],
 })
 export class TogglePanelModule {}
