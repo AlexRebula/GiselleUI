237151f2c9af4b685be2ad891ab68cec9d22adc9
alexrebula <alex@alexrebula.com>
2021-10-23 10:25:13 +1100

    ADD: Matrix component

--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/organisms/matrix/matrix.component.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/organisms/matrix/matrix.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+    IButtonIconSizeEnum,
+    IFontAwesomeIconEnum,
+} from '../../atoms/button/_interfaces/button.enums';
+import { IICon } from '../../atoms/icon/_interfaces/icon.interfaces';
+import { UtilityService } from '../../_services/utility.service';
 
 @Component({
     selector: 'gee-matrix',
@@ -6,7 +12,97 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./matrix.component.scss'],
 })
 export class MatrixComponent implements OnInit {
-    constructor() {}
+    showActivityClassArray = [];
+    @Input()
+    matrixHeaders: any;
+    @Input()
+    defaultColumns: any;
+    @Input()
+    items: any;
+    constructor(private utilityService: UtilityService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.showActivityClassArray = this.utilityService.deepCloneObject(
+            this.defaultColumns
+        );
+    }
+
+    getArrayFromCommaSeparatedString(
+        items: string | undefined
+    ): string[] | undefined {
+        let result: string[] | undefined = [];
+
+        result = items?.split(',');
+
+        return result;
+    }
+
+    getActivityIcon(activity: string): IICon {
+        let checkIcon: IICon = {
+            size: IButtonIconSizeEnum.fa1x,
+            name: IFontAwesomeIconEnum.faCheck,
+        };
+
+        let faSpa: IICon = {
+            size: IButtonIconSizeEnum.fa1x,
+            name: IFontAwesomeIconEnum.faSpa,
+        };
+
+        switch (activity.toLowerCase()) {
+            case 'y':
+                return checkIcon;
+            default:
+                return checkIcon;
+        }
+    }
+
+    getActivityCSSName(index: number): string {
+        let headersArray = this.matrixHeaders.split(',');
+        let result;
+        if (headersArray && headersArray[index]) {
+            result = headersArray[index]
+                .toLowerCase()
+                .trim()
+                .replace('/', '')
+                .replace('&', '')
+                .replace(' ', '-')
+                .replace('  ', '-')
+                .replace(' ', '-')
+                .replace('--', '');
+            // console.log(result);
+        }
+
+        if (result) return result;
+        else return '';
+    }
+
+    onHideColumnButtonClick(index: number) {
+        let headersArray = this.matrixHeaders.split(',');
+        let itemToRemove = 'show__';
+        let cssSuffix = '';
+        if (headersArray) {
+            cssSuffix = headersArray[index]
+                .toLowerCase()
+                .trim()
+                .replace('/', '')
+                .replace('&', '')
+                .replace(' ', '-')
+                .replace('  ', '-')
+                .replace(' ', '-')
+                .replace('--', '');
+
+            itemToRemove = itemToRemove + cssSuffix;
+
+            this.showActivityClassArray = this.showActivityClassArray.filter(
+                (e) => e !== itemToRemove
+            );
+            // console.log(result);
+        }
+    }
+
+    onShowAllColumnButtonClick() {
+        this.showActivityClassArray = this.utilityService.deepCloneObject(
+            this.defaultColumns
+        );
+    }
 }
--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/organisms/matrix/matrix.module.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/organisms/matrix/matrix.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatrixComponent } from './matrix.component';
+import { IconModule } from '../../atoms/icon/icon.module';
 
 @NgModule({
     declarations: [MatrixComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, IconModule],
     exports: [MatrixComponent],
 })
 export class MatrixModule {}
