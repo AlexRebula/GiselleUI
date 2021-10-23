48730824cfa35e158409fd9efbcefa7f2744fbd7
alexrebula <alex@alexrebula.com>
2021-10-23 11:04:36 +1100

    ADD: Details template component

--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/details-template/details-template.component.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/details-template/details-template.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IFormSection } from '../../organisms/form/_interfaces/form.interfaces';
+import { IMenuList } from '../menu-template/_interfaces/menu.interfaces';
+import { IDetails } from './_interface/details.interfaces';
+import { DetailsService } from './_services/details.service';
 
 @Component({
     selector: 'gee-details-template',
@@ -6,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./details-template.component.scss'],
 })
 export class DetailsTemplateComponent implements OnInit {
-    constructor() {}
+    @Input()
+    config: IDetails | undefined;
+
+    @Input()
+    detailsFormSections: IFormSection[] | undefined;
+
+    menuLists: IMenuList[] | undefined = [];
+
+    constructor(private detailsService: DetailsService) {}
 
     ngOnInit(): void {}
 }
--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/details-template/details-template.module.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/details-template/details-template.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsTemplateComponent } from './details-template.component';
+import { SectionModule } from '../../organisms/section/section.module';
+import { FormModule } from '../../organisms/form/form.module';
+import { MenuListTemplateModule } from '../menu-list-template/menu-list-template.module';
+import { MenuTemplateModule } from '../menu-template/menu-template.module';
+import { MatrixModule } from '../../organisms/matrix/matrix.module';
 
 @NgModule({
     declarations: [DetailsTemplateComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        SectionModule,
+        FormModule,
+        MenuListTemplateModule,
+        MenuTemplateModule,
+        MatrixModule,
+    ],
     exports: [DetailsTemplateComponent],
 })
 export class DetailsTemplateModule {}
