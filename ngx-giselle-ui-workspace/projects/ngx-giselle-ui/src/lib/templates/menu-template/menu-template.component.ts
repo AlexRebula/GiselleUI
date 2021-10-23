b0b918a3c47c5138499fb999a53dbda1edabb2f7
alexrebula <alex@alexrebula.com>
2021-10-23 11:07:11 +1100

    ADD: Menu template component

--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/menu-template/menu-template.component.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/menu-template/menu-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'gee-menu-template',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./menu-template.component.scss'],
 })
 export class MenuTemplateComponent implements OnInit {
+    @Input()
+    lists: any[] | undefined;
+
     constructor() {}
 
     ngOnInit(): void {}
--- ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/menu-template/menu-template.module.ts
+++ ngx-giselle-ui-workspace/projects/ngx-giselle-ui/src/lib/templates/menu-template/menu-template.module.ts
@@ -1,10 +1,11 @@
+import { MenuListTemplateModule } from './../menu-list-template/menu-list-template.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuTemplateComponent } from './menu-template.component';
 
 @NgModule({
     declarations: [MenuTemplateComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MenuListTemplateModule],
     exports: [MenuTemplateComponent],
 })
 export class MenuTemplateModule {}
